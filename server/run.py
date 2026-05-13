e76a755340174ad07320ae3aea569bff9546842f
Patricia Mwangi <patricia.mwangi@student.moringaschool.com>
2026-05-13 21:21:15 +0300

    port

--- server/run.py
+++ server/run.py
@@ -3,8 +3,12 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # Use the PORT environment variable provided by Render, defaulting to 5000
-    port = int(os.environ.get("PORT", 5000))
+    # host='0.0.0.0' makes it accessible on your local network
+    app.run(debug=True, port=5000)
+
+    # if __name__ == '__main__':
+    # # Use the PORT environment variable provided by Render, defaulting to 5000
+    # port = int(os.environ.get("PORT", 5000))
     
     # CRITICAL: Change host to '0.0.0.0' for cloud deployment
     app.run(host='0.0.0.0', port=port)
